b071a8f69166304bd77d29f3827b2f08e9108a49
Jon Vallet <jon.vallet@nwboxed.com>
2024-05-23 10:05:52 +0100

    Add basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use crate::server::build;
+use actix_web::{App, HttpServer, web};
+
+use crate::server::{echo, hello, info};
 
 mod server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    build().await
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(info)
+            .route("hey", web::get().to(crate::server::manual_hello))
+    }).bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, get, HttpResponse, HttpServer, post, Responder,
+use actix_web::{get, HttpResponse, post, Responder,
                 Result, web};
 use serde::{Deserialize, Serialize};
 
@@ -27,20 +27,8 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
+pub(crate) async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
-}
-
-pub async fn build() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .service(info)
-            .route("hey", web::get().to(manual_hello))
-    }).bind(("127.0.0.1", 8080))?
-        .run()
-        .await
 }
 
 #[cfg(test)]
